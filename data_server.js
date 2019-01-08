7481730a9e2d6fe1f99c0ec9cd886f88b8196f88
Stephen Andryc <Stephen_Andryc@Stephens-MacBook-Air.local>
2019-01-08 12:10:03 -0500

    Winterbreakwasnotreallyproductiveintheendafterall

--- data_server.js
+++ data_server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var fs = require('fs');
 var favicon = require('serve-favicon');
 
-
 var app = express();
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
@@ -12,7 +11,7 @@ app.use(favicon(__dirname + '/public/images/logo.png'));
 var port = 3000;
 app.listen(port, function(){
   console.log('Server started at '+ new Date()+', on port ' + port+'!');
-});
+});//console confirmation of established server
 
 app.get('/', function(request, response){
   response.status(200);
@@ -20,48 +19,55 @@ app.get('/', function(request, response){
   response.render('index');
 });
 
+var userStringToObject = function (userArray) {
+    var output = {};//initiates object that will be the user
+    output["name"] = userArray[0];
+    output["gamesPlayed"] = parseInt(userArray[1]);
+    output["wins"] = parseInt(userArray[2]);
+    output["losses"] = parseInt(userArray[3]);
+    output["paper"] = parseInt(userArray[4]);
+    output["rock"] = parseInt(userArray[5]);
+    output["scissors"] = parseInt(userArray[6]);
+    output["password"] = userArray[7];
+    //adds object attributes dependent on index in array
+    return output;//returns object as output
+}
+
 app.get('/login', function(request, response){
   var user_data={
       name: request.query.player_name,
       password: request.query.player_password
-  };
-  var users_file=fs.readFileSync('data/users.csv','utf8');
-  var rows = users_file.split('\n');
-  var user_info = [];
-  for(var i=1; i<rows.length-1; i++){
-    var user_d = rows[i].split(',');
-    var user = {};
-    user["name"] = user_d[0];
-    user["password"] = user_d[7];
-    user_info.push(user);
+  };//reads data fields
+  var users_file=fs.readFileSync('data/users.csv','utf8');//converts users csv to a string
+  var rows = users_file.split('\n');//generates array of stringified user objects
+  var user_info = [];//array which will hold objectified users
+  for(var i=1; i<rows.length-1; i++){//indexing does not include header or whitespace at the end
+    var user = userStringToObject(rows[i].split(','));//converts stringified user object to array of stringified values
+    user_info.push(user);//adds user to list
   }
-  //console.log(user_data["password"]);
+
+  var newUserInfo = true;//boolean which tells if new or current user
   for(i=0;i<user_info.length;i++){
     if(user_info[i]["name"]==user_data["name"]){
-
       if(user_info[i]["password"]==user_data["password"]){
         response.status(200);
         response.setHeader('Content-Type', 'text/html')
         response.render('game', {user:user_data});
-      }
-
-      else{
-          document.getElementById("feedback").classList.remove("hidden");
-          document.getElementById("feedback").classList.add("visible");
-
-          //console.log(user_info[i]["password"] + " " + user_data["password"]);
-
+      } else {
+        document.getElementById("feedback").classList.remove("hidden");
+        document.getElementById("feedback").classList.add("visible");
         //error message
+        //console.log(user_info[i]["password"] + " " + user_data["password"]);//tool for console to compare passwords, obvious security flaw for final project
+        
         //regenerate index, insert username and password
       }
-
+      newUserInfo = false;
     }
-    else{
-      //create new user and send to csv
-    }
-
   }
 
+  if (newUserInfo) {
+    //adds new user and goes to game page, maybe later make it new page?
+  }
 });
 
 app.get('/:user/results', function(request, response){
@@ -90,8 +96,10 @@ app.get('/stats', function(request, response){
   //parse the csv
   var rows = users_file.split('\n');
   var villainsRows = villains_file.split('\n');
+  //converts strings into arrays
   console.log(rows);
   console.log(villainsRows);
+  //logs to developer console for reference
   var user_data = [];
   var villain_data = [];
   for(var i=1; i<rows.length-1; i++){
@@ -115,13 +123,13 @@ app.get('/stats', function(request, response){
     var villain_d = villainsRows.split(',');
     console.log(villain_d);
     var villain = {};
-    villain["name"] = user_d[0];
-    villain["gamesPlayed"] = parseInt(user_d[1]);
-    villain["wins"] = parseInt(user_d[2]);
-    villain["losses"] = parseInt(user_d[3]);
-    villain["paper"] = parseInt(user_d[4]);
-    villain["rock"] = parseInt(user_d[5]);
-    villain["scissors"] = parseInt(user_d[6]);
+    villain["name"] = villain_d[0];
+    villain["gamesPlayed"] = parseInt(villain_d[1]);
+    villain["wins"] = parseInt(villain_d[2]);
+    villain["losses"] = parseInt(villain_d[3]);
+    villain["paper"] = parseInt(villain_d[4]);
+    villain["rock"] = parseInt(villain_d[5]);
+    villain["scissors"] = parseInt(villain_d[6]);
     villain_data.push(villain);
   }
   console.log(user_data);
